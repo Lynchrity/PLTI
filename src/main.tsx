55fb303344ddbfb819ae449f60a2268a1b10b2a4
Frederick Samuel <frederick.samuel@binus.edu>
2026-06-09 07:55:23 +0700

    fix: Added app name and changed admin fees

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { APP_NAME } from './constants/app'
 import { applyLogoFavicon } from './icons/LogoMark'
 import './index.css'
 import App from './App.tsx'
 
 applyLogoFavicon()
+document.title = APP_NAME
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
